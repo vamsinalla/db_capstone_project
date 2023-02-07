e0a1ec4153aab529e35d302b380a74363906178e
RaghuVamsi Nalla <raghuvamsinalla@RaghuVasMacBook.home>
2023-02-07 02:57:09 -0500

    Week two part 4 completed

---
+++ weeek-2updatebooking.sql
@@ -0,0 +1,8 @@
+DELIMITER $$ 
+CREATE PROCEDURE UpdateBooking(IN BookingID INT, IN BookingDate DATE) 
+BEGIN
+UPDATE bookings SET date = BookingDate WHERE booking_id = BookingID; 
+SELECT CONCAT("Booking ", BookingID, " updated") AS "Confirmation"; 
+END$$ 
+DELIMITER ; 
+CALL UpdateBooking(9, "2022-12-17");
---
+++ week-2addbookings.sql
@@ -0,0 +1,7 @@
+DELIMITER $$ 
+CREATE PROCEDURE AddBooking(IN BookingID INT, IN CustomerID INT, IN TableNumber INT, IN BookingDate DATE)
+BEGIN 
+INSERT INTO bookings (booking_id, customer_id, table_number, date) VALUES (BookingID, CustomerID, TableNumber, BookingDate); 
+SELECT "New booking added" AS "Confirmation";
+END$$ DELIMITER ; 
+CALL AddBooking(9, 3, 4, "2022-12-30");
