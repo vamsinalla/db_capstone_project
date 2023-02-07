8f691e8416b5fcb8f7ec49753e1230f9d8009524
RaghuVamsi Nalla <raghuvamsinalla@RaghuVasMacBook.home>
2023-02-07 02:51:25 -0500

    Week two part 3 completed

---
+++ week-2-insert.sql
@@ -0,0 +1,10 @@
+
+
+use LittleLemonDB;
+INSERT INTO Bookings (BookingID,Date, TableNumber, CustomerID)
+VALUES 	(1,"2022-10-10", 5, 1),
+		(2,"2022-11-12", 3, 3),
+		(3,"2022-10-11", 2, 2),
+		(4,"2022-10-13", 2, 1);
+        
+SELECT * FROM bookings;
---
+++ week-2checkbooking.sql
@@ -0,0 +1,4 @@
+CREATE PROCEDURE CheckBooking(BookingDate DATE,Table_Number INT)
+	SELECT CONCAT("Table ", TableNumber, " is already booked")
+    WHERE exists (select * from Bookings where Date = BookingDate and TableNumber = Table_Number);
+CALL CheckBooking("2022-11-12", 3);
